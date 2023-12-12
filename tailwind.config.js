871397ac4c6f9c51d7686db8c3134cf48063b693
Alex Bolotsov <mamoruuu@proton.me>
2023-12-12 23:32:02 +0600

    feat: adds the accent color to tailwind colour pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
 	theme: {
 		colors: require('@evilmartians/harmony/tailwind'),
 		extend: {
+			colors: {
+				accent: {
+					'50': 'oklch(0.988 0.2 var(--accent-hue))',
+					'100': 'oklch(0.966 0.2 var(--accent-hue))',
+					'200': 'oklch(0.922 0.2 var(--accent-hue))',
+					'300': 'oklch(0.859 0.2 var(--accent-hue))',
+					'400': 'oklch(0.798 0.2 var(--accent-hue))',
+					'500': 'oklch(0.738 0.2 var(--accent-hue))',
+					'600': 'oklch(0.625 0.2 var(--accent-hue))',
+					'700': 'oklch(0.533 0.2 var(--accent-hue))',
+					'800': 'oklch(0.418 0.2 var(--accent-hue))',
+					'900': 'oklch(0.306 0.2 var(--accent-hue))',
+					'950': 'oklch(0.195 0.2 var(--accent-hue))',
+				}
+			},
 			screens: {
 				'3xl': '1750px',
 				'4xl': '2000px'
